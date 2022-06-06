bb79a32eb5ef5c66d3e199acc184058970f5728c
Ian Wiggins <wiggins.ian.r@gmail.com>
2022-06-06 21:22:53 +0100

    parsing request bodies

--- app.js
+++ app.js
@@ -10,15 +10,25 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input type="text"><button type="submit" name="message">Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    console.log(req);
-    fs.writeFileSync("message.txt", "Another dummy message");
-    res.writeHead(302, { Location: "/" });
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log("chunk", chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1].replaceAll("+", " ");
+
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
